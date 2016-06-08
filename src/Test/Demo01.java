00fed87900df46479250b0e595c7e26aaf6e61e8
wangxuan <wangxuan3060@163.com>
2016-06-08 10:57:41 +0800

    the lastest commit master

--- src/Test/Demo01.java
+++ src/Test/Demo01.java
@@ -5,6 +5,7 @@ public class Demo01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git & World£¡");
+		System.out.println("Delete feature1 and commit master");
 	}
 
 }
